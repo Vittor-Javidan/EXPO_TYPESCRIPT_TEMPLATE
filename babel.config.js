1e30aa82eac9fdae80339c5af98e244950824fb4
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-04-05 22:56:42 -0300

    jest install babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+      '@babel/preset-typescript',
+    ],
     plugins: [
       // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
       require.resolve("expo-router/babel"),
