b4991f6ccdf844176f8388ce11c30056ecf330a7
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-04-07 16:54:36 -0300

    Reanimated and Moti installed

--- app/index.tsx
+++ app/index.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import {Link} from 'expo-router';
 import {Text, View} from 'react-native';
 import {styles} from './styles';
+import {MotiView} from 'moti';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+    <MotiView
+      style={styles.motiView}
+      from={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{type: 'timing', duration: 500}}>
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <Text style={styles.title}>Hello World</Text>
+          <Text style={styles.subtitle}>
+            This is the first page of your app.
+          </Text>
+        </View>
+        <Link href={'/Page2'} style={styles.link}>
+          Page 2
+        </Link>
       </View>
-      <Link href={'/Page2'} style={styles.link}>
-        Page 2
-      </Link>
-    </View>
+    </MotiView>
   );
 }
